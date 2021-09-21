a2745d8a5705231535ebeb7b39f33c4c956d9def
Junming Zhao <junming.zhao@anu.edu.au>
2021-09-21 23:00:12 +1000

    implemented sketch of Strassen

---
+++ BLAS.cpp
@@ -0,0 +1,28 @@
+// MATH3512 Matrix Computations, The Australian National University
+// Supervisor: Professor Linda Stals
+// Student : u6633756 Junming Zhao
+
+// BLAS.cpp - implementation of BLAS3 operation (a) and (d)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+/** BLAS level3 function a): C = alpha*A*B + beta*C
+    ---- parameters ----
+    [in] alpha : scalar
+    [in] beta  : scalar
+    [in] m : int
+    [in] n : int
+    [in] p : int
+    [in] A : m x n matrix
+    [in] B : n x p matrix
+    [in,out] C : m x p matrix
+**/
+void BLAS_3A(int alpha, int beta, int m, int n, int p,
+            vector<vector <int>> A,
+            vector<vector <int>> B,
+            vector<vector <int>> &C){
+
+}
---
+++ test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector<vector <int> > Matrix_Slice(int m1, int m2, int n1, int n2, vector<vector <int> > A){
+    vector<vector <int> > Matrix_Out;
+
+    for (int i=m1; i<m2; i++){
+        Matrix_Out.emplace_back(A[i].begin()+n1, A[i].begin()+n2);
+    }
+    return Matrix_Out;
+}
+
+
+int main(){
+    int m = 4;
+    int n = 5;
+    vector<vector <int> > A1 = {{1,2,3,4,5,6}, {4,5,6,7,8,9}, {1,3,4,6,8,0}, {0,5,7,8,1,2}};
+    vector<vector <int> > A2 = Matrix_Slice(m/2, m, n/2, n, A1);
+
+
+    // A2.reserve(2);
+
+    for (unsigned int i=0; i<A2.size(); i++){
+        for (unsigned int j=0; j<A2[i].size(); j++){
+            // cout << A1[i][j] << " ";
+            cout << A2[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
