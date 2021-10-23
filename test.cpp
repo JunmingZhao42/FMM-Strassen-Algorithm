074b4feacdeb6074afc04a967e60990cdc1a9bee
Junming Zhao <junming.zhao@anu.edu.au>
2021-10-24 08:26:30 +1100

    test data

--- test.cpp
+++ test.cpp
@@ -7,57 +7,82 @@
 
 
 #include "blas_routine.cpp"
+// #include "matrix.cpp"
 #include <iostream>
 #include <chrono>
 
 
+const int REPEAT = 5;
+const int SIZE = 1000;
+const int STEP = 10;
+
+
 int main(){
+    // // test for Strassen algorithm
+    // std::cout << "matrix dimension," << "Strassen," << "Standard" << std::endl;
+    // for (int m=0; m<SIZE; m+=STEP){
+    //     for (int i=0; i<REPEAT; i++){
+    //         std::cout << m << ",";
+    //         // create matrix A
+    //         Matrix A = Matrix(m);
+    //         A.assign_random();
+
+    //         // create matrix B
+    //         Matrix B = Matrix(m);
+    //         B.assign_random();
+
+    //         std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    //         // C = A*B
+    //         Matrix C = Matrix::strassen(A,B);
+    //         std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    //         std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << ",";
+
+    //         // standard matrix multiplication
+    //         begin = std::chrono::steady_clock::now();
+    //         C = A*B;
+    //         end = std::chrono::steady_clock::now();
+
+    //         std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << std::endl;
+    //     }
+    // }
+    
+
+    // test for BLAS3-A routine
     std::cout << "matrix dimension," << "Strassen," << "Standard" << std::endl;
+    
+    for (int m=0; m < SIZE; m+=STEP){
+        for (int i=0; i<REPEAT; i++){
+            std::cout << m << ",";
+            // create matrix A
+            Matrix A = Matrix(m);
+            A.assign_random();
 
-    for (int m=100; m < 500; m+=10){
-        std::cout << m << ",";
-        // create matrix A
-        Matrix A = Matrix(m);
-        A.assign_random();
+            // create matrix B
+            Matrix B = Matrix(m);
+            B.assign_random();
 
-        // create matrix B
-        Matrix B = Matrix(m);
-        B.assign_random();
+            // create matrix C
+            Matrix C(m);
 
-        // create matrix C
-        Matrix C(m);
+            std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+            // C = 2.0*A*B + 2.0*C
+            BLAS_3A(2.0, 2.0, A, B, C);
+            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
-        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-        // C = 2.0*A*B + 2.0*C
-        BLAS_3A(2.0, 2.0, A, B, C);
-        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+            std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << ",";
 
-        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << ",";
+            // standard matrix multiplication
+            begin = std::chrono::steady_clock::now();
+            C.assign_zeros();
+            // C = 2.0*A*B + 2.0*C
+            C *= 2.0;
+            C += A*B;
+            end = std::chrono::steady_clock::now();
 
-        // standard matrix multiplication
-        begin = std::chrono::steady_clock::now();
-        C.assign_zeros();
-        // C = 2.0*A*B + 2.0*C
-        C *= 2.0;
-        C += A*B;
-        end = std::chrono::steady_clock::now();
-
-        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << std::endl;
+            std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << std::endl;
+        }
     }
 
-    
-    // Matrix matrix4 = matrix1 * matrix2;
-    // matrix4.print();
-
-
-    // blas 3d test
-    // Matrix id3(m);
-    // id3.print();
-    // Matrix m1 = Matrix(m,m);
-    // m1.assign_random();
-    // m1.print();
-
-    // Matrix m2 = BLAS_3D(1, m1, id3);
-    // m2.print();
     return 0;
 }
